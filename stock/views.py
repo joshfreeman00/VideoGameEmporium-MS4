99d8a421739e0282930ad4c5df027659304a8f30
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-10-19 16:13:08 +0000

    Implement a view for adding products

--- stock/views.py
+++ stock/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
 
-# Create your views here.
+from .models import Product, Category
+from .forms import ProductForm
+
+def add_product(request):
+    ''' Add a product to the store '''
+    form = ProductForm()
+    template = 'stock/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
