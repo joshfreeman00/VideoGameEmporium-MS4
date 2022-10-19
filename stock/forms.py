31bbc53d402285150e9b6a71241594f145603943
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-10-19 13:51:53 +0000

    Create a product form

---
+++ stock/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Product, Category
+
+class PRoductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        display_names = [(c.id, c.get_display_name()) for c in categories]
+
+        self.fields['category'].choices = display_names
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black bg-white rounded-0'
