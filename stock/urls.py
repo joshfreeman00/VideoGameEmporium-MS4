97d610a72ae8363d334a765662c25d7370cd5dda
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-10-19 17:50:00 +0000

    Add a path for editing products

--- stock/urls.py
+++ stock/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add/', views.add_product, name='add_product')
+    path('add/', views.add_product, name='add_product'),
+    path('edit/<int:product_id>', views.edit_product, name='edit_product')
 ]
