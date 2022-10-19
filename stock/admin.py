792e72a9645895b59cc88014e2c57e22882b6cbc
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-10-19 13:26:30 +0000

    Create models for both stock and categoires

---
+++ stock/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Stock, Category
+
+admin.site.register(Stock)
+admin.site.register(Category)
---
+++ stock/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+class Category(models.Model):
+    ''' Model for the stock categories '''
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    display_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        ''' Changes the default object name '''
+        return self.name
+
+    def get_display_name(self):
+        ''' Function to return the display name '''
+        return self.display_name
+
+
+class Stock(models.Model):
+    category = models.ForeignKey("Category",
+                                 null=True,
+                                 blank=True,
+                                 on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
