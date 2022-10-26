9cfec4e5ab509c28e8dd52d516bb3b7a060cb9fb
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-10-26 15:23:53 +0000

    Implement cart context and delivery logic

--- vg_emporium/settings.py
+++ vg_emporium/settings.py
@@ -80,6 +80,7 @@ TEMPLATES = [
                 'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context.cart_contents',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
@@ -163,3 +164,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = (os.path.join(BASE_DIR, 'media'))
+
+FREE_DELIVERY_THRESHOLD = 20
+STANDARD_DELIVERY_PERCENTAGE = 5
