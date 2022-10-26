c443146a51460a0b7b816c0b96ef6e730fb7be48
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-10-26 10:05:31 +0000

    Implement category filtering functionality

--- stock/views.py
+++ stock/views.py
@@ -13,8 +13,14 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -27,6 +33,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'selected_categories': categories,
     }
 
     return render(request, 'stock/products.html', context)
