d29bd95f3b2d4dde5a16ef4e0807eec62fd42b31
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-10-29 15:36:33 +0000

    Update cart context processor

--- cart/urls.py
+++ cart/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.display_cart, name='display_cart')
+    path('', views.display_cart, name='display_cart'),
+    path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -5,3 +5,19 @@ def display_cart(request):
     ''' A view to render the shopping cart '''
 
     return render(request, 'cart/shopping_cart.html')
+
+
+def add_to_cart(request, item_id):
+    ''' Add a quantity of a product to the cart '''
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+        cart[item_id] = quantity
+
+    request.session['cart'] = cart
+    return redirect(redirect_url)
