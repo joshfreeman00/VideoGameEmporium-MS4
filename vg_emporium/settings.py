1da5bf3ced38f8cb03c4f01f72a22602e34d6a23
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-11-02 14:56:29 +0000

    Create checkout app

--- vg_emporium/settings.py
+++ vg_emporium/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'stock',
     'profiles',
     'cart',
+    'checkout',
 
     # other
     'crispy_forms',
@@ -77,7 +78,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.media',
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', # required by allauth
+                'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'cart.context.cart_contents',
