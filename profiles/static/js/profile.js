c011b2514d731eb3f76201808d2232d0e27edf82
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-11-05 14:29:30 +0000

    Add a script for the country form input

---
+++ profiles/static/js/profile.js
@@ -0,0 +1,12 @@
+let countrySelected = $('#id_default_country').val();
+if(!countrySelected) {
+    $('#id_default_country').css('color', '#b6c0cb');
+};
+$('#id_default_country').change(function() {
+    countrySelected = $(this).val();
+    if(!countrySelected) {
+        $(this).css('color', '#b6c0cb');
+    } else {
+        $(this).css('color', '#000');
+    }
+});
