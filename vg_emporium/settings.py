0b2b3286ccad3d7c526592d566d2325c721b9754
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-11-06 16:05:26 +0000

    Deploy to heroku

--- vg_emporium/settings.py
+++ vg_emporium/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['video-game-emporium-ms4.herokuapp.com']
+ALLOWED_HOSTS = ['video-game-emporium-ms4.herokuapp.com', 'localhost']
 
 
 # Application definition
