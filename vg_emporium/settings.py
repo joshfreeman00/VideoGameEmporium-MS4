be1683da9453d2790274d67fa4a4ad2444c28a29
joshfreeman00 <josh2000freeman@aol.co.uk>
2022-11-11 12:40:22 +0000

    Fix gmail email config settings

--- vg_emporium/settings.py
+++ vg_emporium/settings.py
@@ -206,6 +206,6 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.googlemail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_ADDRESS')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-    DEFAULT_EMAIL_ADDRESS = os.environ.get('EMAIL_HOST_ADDRESS')
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
